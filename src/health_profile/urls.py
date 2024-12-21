65b0b5bb007fbfb39bd4805cd6a6776fc2b2c3b6
add <1395918522@qq.com>
2024-12-21 23:00:48 +0800

    feat(health_profile): 增加了显示所有健康信息以及健康信息删除功能

--- src/health_profile/urls.py
+++ src/health_profile/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.health_profile_list.as_view(), name='health_profile_list'),
     path('add/', views.add_health_profile.as_view(), name='add_health_profile'),
-    # path('delete/<int:profile_id>/', views.delete_health_profile, name='delete_health_profile'),
+    path('delete/<int:health_profile_id>/', views.delete_health_profile.as_view(), name='delete_health_profile'),
 ]
